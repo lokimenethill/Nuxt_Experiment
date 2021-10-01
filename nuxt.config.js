ed5f1a104f4504ce2a939dc34f245ce6ce108d91
lokimenethill <you@example.com>
2021-09-30 19:57:54 -0500

    test_pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ import EnglishLang from './locales/en.json'
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
-
+  target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - prueba1',
