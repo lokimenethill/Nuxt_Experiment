2acbecc7b030e7b3296e0f4cd86a2665f246c94d
lokimenethill <xdjtrancemetal@hotmail.com>
2021-11-07 03:30:51 -0600

    update_eslit

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,10 +8,12 @@ module.exports = {
     parser: '@babel/eslint-parser',
     requireConfigFile: false,
   },
-  extends: ['@nuxtjs', 'plugin:nuxt/recommended'],
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended',"prettier"],
   plugins: [],
   // add your custom rules here
   rules: {
     'comma-dangle': ['error', 'always-multiline'],
+    'space-before-function-paren':['error','never'],
+    semi:['error','always']
   },
 }
