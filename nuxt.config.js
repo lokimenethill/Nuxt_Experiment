e3bcf3fdce51cdb1dec06f7ffa12546f59727934
lokimenethill <xdjtrancemetal@hotmail.com>
2021-11-21 21:17:42 -0600

    rebuild-config

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,6 +23,26 @@ export default {
         type: 'image/x-icon',
         href: '/generals/logo-mesolex-favicon-01.svg',
       },
+      {
+        rel:"stylesheet",
+        href:"https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
+      },
+      {
+        rel:"preconnect",
+        href:"https://fonts.googleapis.com",
+      },
+      {
+        rel:"preconnect", 
+        href:"https://fonts.gstatic.com",
+      },
+      {
+        rel:"stylesheet",
+        href:"https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp",
+      },
+      {
+        re:"manifest",
+        href:"../imagenes-generales/favicon_package/site.webmanifest",
+      }
     ],
   },
 
