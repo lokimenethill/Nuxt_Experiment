ecd199ce126da6336319ae8058741c630d0dae23
lokimenethill <xdjtrancemetal@hotmail.com>
2022-01-27 18:02:08 -0600

    lib

---
+++ components/libraryComponents/sBar.vue
@@ -0,0 +1,273 @@
+<template lang="">
+     <div class="contenedor-general-rebasado">
+      <div class="contenedor-general">
+        <div class="contenedor-breadcums"><a class="breadcums">Library</a>
+          <p class="breadcums">/</p><a href="" class="breadcums-activo">Mixtec</a>
+        </div>
+        <h6>Search in</h6>
+        <h1 id="titulo-library">Library</h1>
+
+
+
+
+        <div class="contenedor-buscador-general">
+         
+            <div class="caja-busqueda">
+              <!-- Selector personalizado https://www.w3schools.com/howto/howto_custom_select.asp -->
+              <!-- Checar por qué se elimina la primera opción -->
+              <div class="dropdown-libreria ">
+                 <v-select :searchable="false" class="style-library" v-model="selected_datalist_first" :options="datalist_first"></v-select>
+              </div>
+              <input type="search" placeholder="Type words for search" class="input-caja-busqueda">
+            </div>
+
+
+
+
+
+            <div class="contenedor-general-botones-busqueda-library ">
+              <div class="contenedor-botones-izquierda ">
+                <button type="reset" id="btn-reset-lexico-buscador-library" class="btn-secundario">Reset</button>
+              </div>
+              <div class="contenedor-botones-derecha  ">
+                <button type="submit" id="btn-search-lexico-buscador-library">Search</button>
+              </div>
+            </div>
+         
+        </div>
+        <div class="contenedor-general-resultados">
+          <h4 class="instrucciones">{#} results. (Page {#} de {#})</h4>
+          <!-- Contenedor de resulatdos, cada tarjeta es un resultado -->
+          <div class="table ">
+            <div class="table__header ">
+              <div class="table__header__row ">
+                <div class="table__header__row__cell  ">
+                  <h4 class="table__header__row__cell__title">Title</h4>
+                  <div class="table__header__row__cell__switch ">
+                    <button @click="sortByTitle()"  class="table__header__row__cell__switch__btn__asc-active"></button>
+                    <button class="table__header__row__cell__title__btn__des"></button>
+                  </div>
+                </div>
+                <div class="table__header__row__cell  ">
+                  <h4 class="table__header__row__cell__title">Author</h4>
+                  <div class="table__header__row__cell__switch">
+                    <button class="table__header__row__cell__switch__btn__asc"></button>
+                    <button class="table__header__row__cell__title__btn__des"></button>
+                  </div>
+                </div>
+                <div class="table__header__row__cell  ">
+                  <h4 class="table__header__row__cell__title">Terminal language</h4>
+                  <div class="table__header__row__cell__switch">
+                    <button class="table__header__row__cell__switch__btn__asc"></button>
+                    <button class="table__header__row__cell__title__btn__des"></button>
+                  </div>
+                </div>
+                <div class="table__header__row__cell  ">
+                  <h4 class="table__header__row__cell__title">Topics</h4>
+                  <div class="table__header__row__cell__switch">
+                    <button class="table__header__row__cell__switch__btn__asc"></button>
+                    <button class="table__header__row__cell__title__btn__des"></button>
+                  </div>
+                </div>
+                <div class="table__header__row__cell  ">
+
+                </div>
+              </div>
+            </div>
+            <div class="table__main">
+              <div v-for="(find, index) in JsonLib" :key="index" :v-bind="index" >
+              <div class="table__main__row">
+                <div class="table__main__row__cell ">
+                  <a href="#" taget="_blank" class="table__main_row__cell__title">{{find.title}}</a>
+                </div>
+                <div class="table__main__row__cell ">
+                  <span class="table__main__row__cell__data">{{find.author}}</span>
+                </div>
+                <div class="table__main__row__cell ">
+                  <span class="table__main__row__cell__data">{{find.terminal_lang}}</span>
+                </div>
+                <div class="table__main__row__cell ">
+                  <span class="table__main__row__cell__data">{{find.topics}}</span>
+                </div>
+                <div class="table__main__row__cell  ">
+                  <button class="table__main__row_cell__button-button_micro"><span
+                      class="material-icons-outlined table__main__row_cell__button-button_micro__icono_boton_descagar_micro">
+                      file_download
+                    </span></button>
+                  <!--Este elemento hace que descargue el documento directamente-->
+                </div>
+                </div>
+              </div>
+            </div>
+            <div class="footer-tabla">
+
+            </div>
+
+          </div>
+
+
+        </div>
+        <!-- Termina contenedor de resultados -->
+        <!-- Inicia contenedor de paginación, esta en ESTILOS GENERALES, ya que este módulo se empleara en varias pantallas -->
+        <div class="contenedor-paginacion ">
+          <p class="informacion-resultados-y-paginas">{#} results. {#} pages.</p>
+          <!-- Contenedor de botones y se planea que aparezcan de 10 en 10 y que la pagina activa este en medio. Es decir si esta en la pagina 24 iniciaria en la 19 y terminaria en la 29 -->
+          <div class="contenedor-paginacion-por-pagina ">
+            <!-- Boton de página anterior -->
+            <button class="btn-pagina-anterior">
+              <!-- Icono boton ágina anterior -->
+              <span class="material-icons-round icono-de-boton-paginacion">
+                arrow_back_ios
+              </span>
+            </button>
+            <!-- Botones de paginas -->
+            <button class="btn-pagina ">1</button>
+            <button class="btn-pagina">2</button>
+            <button class="btn-pagina">3</button>
+            <button class="btn-pagina">4</button>
+            <!-- btn-pagina-activa es un marcador que indica la página en la que se encuentra -->
+            <button class="btn-pagina btn-pagina-activa">5</button>
+            <button class="btn-pagina">6</button>
+            <button class="btn-pagina">7</button>
+            <button class="btn-pagina">8</button>
+            <button class="btn-pagina">9</button>
+            <button class="btn-pagina">10</button>
+            <!-- Boton página siguiente -->
+            <button class="btn-pagina-siguiente">
+              <span class="material-icons-round icono-de-boton-paginacion ">
+                arrow_forward_ios
+              </span>
+            </button>
+          </div>
+          <!-- Termina contenedor de botones por página -->
+
+          <!-- Contenedor de input para navegar directamente a una pagina -->
+          <div class="contenedor-ir-a-pagina ">
+            <!-- Creo que es forzoso usar el formulario -->
+
+            
+              <input type="number" min="0" placeholder="Go to page" class="caja-input-ir-a-pagina"><button type="submit"
+                class="btn-ir-a-pagina"><span class="material-icons-outlined icono-ir-a-pagina">
+                  arrow_forward
+                </span></button>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+import JSonLibrary from "@/static/libraryBooks/books.json";
+export default {
+  
+  data() {
+    return {
+      JsonLib:JSonLibrary,
+      selected_datalist_first: {label:'family',val:'family'},
+      datalist_first: [
+        { label: 'family', val: 'family' },
+        { label: 'genus', val: 'genus' },
+        { label: 'genus species', val: 'genus_species' },
+        { label: 'glosa', val: 'glosa' },
+        { label: 'nombre', val: 'nombre' },
+        { label: 'Significado preciso', val: 'precise_meaning' },
+        { label: 'Significado extendido', val: 'extended_meaning' },
+        { label: 'Frase Ilustrativa', val: 'illustrative_phrases' },
+        { label: 'Búsqueda exhaustiva', val: 'complete_search' },
+      ],
+     
+    };
+  },
+  watch:{
+   
+  },
+  methods: {
+    sortByTitle(){
+      this.JsonLib.sort(function(a, b){
+  if (a.title > b.title){
+    return 1;
+  }
+  if (a.title < b.title){
+    return -1;
+  }
+  // a must be equal to b
+  return 0;
+});
+    },
+  },
+};
+</script>
+<style>
+/* generales dropdown */
+.vs__search{
+  margin: 0px !important;
+  padding: 0px !important;
+font-family: "Fira Sans", sans-serif !important;
+font-size: 0.8888rem !important;
+fill: white;
+
+}
+.vs__dropdown-option{
+   padding: 0rem 0.5rem;
+ 
+}
+/* selector parametros and/or/ornot etc.. */
+.style-library .vs__dropdown-option{
+width: 29rem !important;
+}
+.style-library .vs__search::placeholder {
+  margin: 0px;
+  padding: 0px;
+  width: 29rem !important;
+}
+.style-library .vs__dropdown-toggle{
+background-color: var(--library1);
+border-radius: 0;
+color: white  !important;
+margin-right: 0.5rem;
+flex-basis: 12%;
+width: 29rem !important;
+padding: 0px !important;
+}
+.style-library .vs__dropdown-toggle:hover {
+background-color: var(--library2);
+}
+.style-library .vs__dropdown-menu {
+  position: absolute;
+  font-family: "Fira Sans", sans-serif;
+  font-size: 0.8888rem;
+  flex-basis: 21%;
+  min-width: 29rem !important;
+  
+}
+.style-library .vs__clear{
+display: none;
+}
+.style-library .vs__open-indicator {
+  fill: white !important;
+}
+.style-library .vs__selected{
+  color: white;
+  font-family: "Fira Sans", sans-serif;
+  font-size: 0.8888rem;
+  padding: 0px !important;
+  margin: 0px 0px 0px 0.25rem !important;
+}
+
+.style-library .vs__dropdown-menu{
+  font-family: "Fira Sans", sans-serif;
+  font-size: 0.8888rem;
+  border: 2px solid var(--library1);
+  width: 29rem !important;
+  
+}
+.style-library .vs__dropdown-menu .vs__dropdown-option{
+   padding: 0rem 0rem 0rem .25rem;
+   width: 28.75rem !important;
+   }
+.style-library .vs__dropdown-menu .vs__dropdown-option--highlight {
+background-color: var(--library2);
+}
+
+
+</style>
---
+++ pages/library/index.vue
@@ -0,0 +1,328 @@
+<template>
+<div>
+ <search-Bar/>
+ 
+</div>
+
+</template>
+<script>
+import SearchBar from "@/components/libraryComponents/sBar.vue";
+
+export default {
+      components:{
+    SearchBar,  
+  },
+};
+</script>
+<style>
+#titulo-library {
+  color: var(--library1) !important;
+}
+
+/* Dropdown libreria */
+.dropdown-libreria {
+  position: relative;
+  font-family: "Fira Sans", sans-serif;
+  font-size: 0.8888rem;
+  min-width: 21%;
+  flex-basis: 50%;
+  flex-grow: 1;
+  margin-top: 0.2222rem;
+  margin-bottom: 0.2222rem;
+  margin-right: 0.5rem;
+}
+.dropdown-libreria:focus {
+  outline: 2px solid var(--interfacesp);
+}
+.dropdown-libreria select {
+  display: none; /*hide original SELECT element: */
+}
+.dropdown-libreria .select-selected {
+  background-color: var(--library1);
+}
+.dropdown-libreria .select-selected:hover {
+  background-color: var(--library2);
+}
+/* Style the arrow inside the select element: */
+.select-selected:after {
+  position: absolute;
+  content: "";
+  top: 0.55rem;
+  right: 10px;
+  width: 0;
+  height: 0;
+  border: 6px solid transparent;
+  border-color: #fff transparent transparent transparent;
+}
+/* Point the arrow upwards when the select box is open (active): */
+.select-selected.select-arrow-active:after {
+  border-color: transparent transparent #fff transparent;
+  top: 0.25rem;
+}
+
+/* style the items (options),  */
+.dropdown-libreria .select-items div {
+  color: var(--library1);
+  padding: 0.25rem;
+  border: 1px solid transparent;
+  /* border-color: transparent var(--videos1) transparent var(--videos1); */
+  cursor: pointer;
+}
+/* including the selected item: */
+.dropdown-libreria .select-selected {
+  color: white;
+  padding: 0.25rem;
+  border: 1px solid transparent;
+  /* border-color: transparent transparent var(--videos1) transparent; */
+  cursor: pointer;
+}
+
+/* Style items (options): */
+.dropdown-libreria .select-items {
+  position: absolute;
+  background-color: white;
+  top: 100%;
+  left: 0;
+  right: 0;
+  z-index: 99;
+  border: 1px solid var(--library1);
+}
+
+/* Hide the items when the select box is closed: */
+.dropdown-libreria .select-hide {
+  display: none;
+}
+
+.dropdown-libreria .select-items div:hover,
+.dropdown-libreria .same-as-selected {
+  background-color: var(--library1);
+  color: white !important;
+}
+
+/* BOTONES LIBRERIA */
+#btn-reset-lexico-buscador-library {
+  border-color: var(--library1);
+  color: var(--library1);
+}
+#btn-reset-lexico-buscador-library:hover {
+  background-color: var(--library2);
+  color: white;
+  border-color: var(--library2);
+}
+#btn-search-lexico-buscador-library {
+  background-color: var(--library1);
+}
+#btn-search-lexico-buscador-library:hover {
+  background-color: var(--library2);
+}
+/* BOTONES DE buscador lexico */
+.contenedor-general-botones-busqueda-library {
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  width: 100%;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+}
+.table {
+  margin-bottom: 3rem;
+}
+.table__header {
+  border-bottom: 2px solid var(--gris2);
+}
+.table__header__row {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+}
+.table__header__row__cell {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.33rem 0.33rem 0.33rem 0.75rem !important;
+  flex-grow: 1;
+  flex-basis: 25%;
+}
+.table__header__row__cell:last-of-type {
+  padding-right: 0.33rem !important;
+}
+.table__header__row__cell:first-of-type {
+  padding-left: 0.33rem !important;
+}
+.table__header__row__cell__title {
+  margin: 0 0.3rem 0 0 !important;
+  color: var(--gris2);
+  font-weight: 600 !important;
+  letter-spacing: 0.5px;
+}
+.table__header__row__cell__switch {
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: flex-start;
+  justify-content: center;
+}
+
+.table__header__row__cell__switch__btn__asc {
+  background: none;
+  color: var(--interface1);
+  padding: 0;
+  margin: 0 !important;
+  line-height: 100%;
+  font-size: 1rem;
+}
+.table__header__row__cell__switch__btn__asc::before {
+  content: "△";
+}
+.table__header__row__cell__switch__btn__asc:hover {
+  background: none;
+}
+.table__header__row__cell__switch__btn__asc:hover::before {
+  content: "▲";
+  color: var(--interface0);
+}
+
+.table__header__row__cell__switch__btn__asc-active {
+  background: none;
+  color: var(--interfacesp);
+  padding: 0;
+  margin: 0 !important;
+  line-height: 100%;
+  font-size: 1rem;
+}
+.table__header__row__cell__switch__btn__asc-active:hover {
+  background: none;
+  color: var(--interface0);
+}
+.table__header__row__cell__switch__btn__asc-active::before {
+  content: "▲";
+}
+
+.table__header__row__cell__title__btn__des {
+  background: none;
+  color: var(--interface1);
+  padding: 0;
+  margin: 0 !important;
+  line-height: 100%;
+  font-size: 1rem;
+  transform: rotate(
+    180deg
+  ); /*Se roto 180deg porque los caracteres de triangulo hacia abajo estan mas altos y generan ruido*/
+}
+.table__header__row__cell__title__btn__des:hover {
+  background: none;
+}
+.table__header__row__cell__title__btn__des:hover::before {
+  content: "▲";
+  color: var(--interface0);
+}
+.table__header__row__cell__title__btn__des::before {
+  content: "△";
+}
+
+.table__header__row__cell__title__btn__des-active {
+  background: none;
+  color: var(--interfacesp);
+  padding: 0;
+  margin: 0 !important;
+  line-height: 100%;
+  font-size: 1rem;
+}
+.table__header__row__cell__title__btn__des-active:hover {
+  background: none;
+  color: var(--interface0);
+}
+.table__header__row__cell__title__btn__des-active::before {
+  content: "▲";
+}
+
+.table__main {
+}
+.table__main__row {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: flex-start;
+  align-items: stretch;
+  border-bottom: 1px solid var(--gris3);
+}
+.table__main__row:nth-of-type(even) {
+  background: var(--library3);
+}
+.table__main__row__cell {
+  padding: 1rem 0.75rem;
+  flex-grow: 1;
+  flex-basis: 25%;
+
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-start;
+  align-items: center;
+}
+.table__main__row__cell:last-of-type {
+}
+.table__main__row__cell:first-of-type {
+  padding-left: 0.33rem !important;
+}
+.table__main_row__cell__title {
+  font-family: "Fira Sans Condensed", sans-serif;
+  font-style: italic;
+  font-size: 0.88rem;
+  font-weight: 500;
+  letter-spacing: 0.5px;
+  color: var(--library1) !important;
+}
+.table__main_row__cell__title:hover {
+  color: var(--library2) !important;
+}
+.table__main__row__cell__data {
+  font-family: "Fira Sans Condensed", sans-serif;
+  font-size: 0.88rem;
+}
+
+.table__main__row_cell__button-button_micro {
+  height: 1rem !important;
+  width: 1rem !important;
+  padding: 0 !important;
+  margin: 0 !important;
+  background: var(--library1);
+  align-self: center;
+}
+.table__main__row_cell__button-button_micro:hover {
+  background: var(--library2);
+}
+.table__main__row_cell__button-button_micro__icono_boton_descagar_micro {
+  font-size: 18px !important;
+  margin: 0 !important;
+}
+
+/* Ajustes de anchos */
+/*Las filas de header, main y footer, deben de ir ligadas en tamaño, por eso estan juntas*/
+
+.table__header__row__cell:nth-of-type(1),
+.table__main__row__cell:nth-of-type(1) {
+  flex-basis: 30%;
+  flex-grow: 2;
+}
+.table__header__row__cell:nth-of-type(2),
+.table__main__row__cell:nth-of-type(2) {
+  flex-basis: 25%;
+  flex-grow: 1;
+}
+.table__header__row__cell:nth-of-type(3),
+.table__main__row__cell:nth-of-type(3) {
+  flex-basis: 25%;
+  flex-grow: 1;
+}
+.table__header__row__cell:nth-of-type(4),
+.table__main__row__cell:nth-of-type(4) {
+  flex-basis: calc(20% - 1.36rem);
+  flex-grow: 1;
+}
+.table__header__row__cell:nth-of-type(5),
+.table__main__row__cell:nth-of-type(5) {
+  flex-basis: 1.36rem !important;
+  padding: 1rem 0.33rem 1rem 0 !important;
+  flex-grow: 0;
+}
+
+</style>
